2f67a42773ec695e554407fa5ded0ba39628ad35
filip <filip.prudek@seznam.cz>
2022-12-06 16:48:35 +0100

    solution to first and second part

---
+++ day_5/main.py
@@ -0,0 +1,40 @@
+"""
+            [C]         [N] [R]    
+[J] [T]     [H]         [P] [L]    
+[F] [S] [T] [B]         [M] [D]    
+[C] [L] [J] [Z] [S]     [L] [B]    
+[N] [Q] [G] [J] [J]     [F] [F] [R]
+[D] [V] [B] [L] [B] [Q] [D] [M] [T]
+[B] [Z] [Z] [T] [V] [S] [V] [S] [D]
+[W] [P] [P] [D] [G] [P] [B] [P] [V]
+ 1   2   3   4   5   6   7   8   9 
+ """
+
+import re 
+
+with open ("orders.txt", "r") as data: 
+    orders = [line.rstrip('\n') for line in data]
+
+
+def move_item(number, origin, to):
+  how_many = number
+  while how_many > 0:
+    n = origin.pop()
+    to.append(n)
+    how_many -= 1
+
+stacks = {
+  1: ["W", "B", "D", "N", "C","F", "J"],
+  2: ["P", "Z", "V", "Q", "L", "S", "T"],
+  3: ["P", "Z", "B", "G", "J", "T"],
+  4: ["G", "T", "L", "J", "Z", "B", "H", "C"],
+  5: ["G", "V", "B", "J", "S"],
+  6: ["P", "S", "Q"],
+  7: ["B", "V", "D", "F", "L", "M", "P", "N"],
+  8: ["P", "S", "M", "F", "B", "D", "L", "R"],
+  9: ["V", "D", "T", "R"]
+}
+
+for step in orders:
+  instruction = re.findall
+  
